67997fc68491945909f6167ab871b7e1d7c27b0c
Berk <berk77_@hotmail.com>
2023-04-02 18:58:09 +0300

    commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var field1: UITextField!
+    
+    @IBOutlet weak var field2: UITextField!
+    
+    @IBOutlet weak var answer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func sumClicked(_ sender: Any) {
+        let text : String = field1.text ?? "0"
+        
+        let text2 : String = field2.text ?? "0"
+        
+        if let num = Int(text), let num2 = Int(text2)
+        {
+            answer.text = "\(num + num2)"
+        }else
+        {
+            answer.text = "Lutfen gecerli bir deger giriniz."
+        }
+    }
 
-
+    @IBAction func minusClicked(_ sender: Any) {
+        let text : String = field1.text ?? "0"
+        
+        let text2 : String = field2.text ?? "0"
+        
+        if let num = Int(text), let num2 = Int(text2)
+        {
+            answer.text = "\(num - num2)"
+        }else
+        {
+            answer.text = "Lutfen gecerli bir deger giriniz."
+        }
+    }
+    
+    @IBAction func divClicked(_ sender: Any) {
+        let text : String = field1.text ?? "0"
+        
+        let text2 : String = field2.text ?? "0"
+        
+        if let num = Double(text), let num2 = Double(text2)
+        {
+            answer.text = "\(num / num2)"
+        }else
+        {
+            answer.text = "Lutfen gecerli bir deger giriniz."
+        }
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        let text : String = field1.text ?? "0"
+        
+        let text2 : String = field2.text ?? "0"
+        
+        if let num = Int(text), let num2 = Int(text2)
+        {
+            answer.text = "\(num * num2)"
+        }else
+        {
+            answer.text = "Lutfen gecerli bir deger giriniz."
+        }
+    }
 }
 
